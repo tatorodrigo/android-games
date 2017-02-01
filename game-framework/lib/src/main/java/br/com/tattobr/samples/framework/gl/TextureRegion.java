2c4f0fe47e4a02e638061192298969c3d46a9892
Rodrigo Botelho <tato.rodrigo@gmail.com>
2017-02-01 10:38:10 -0200

    Added width/height to texture region

--- game-framework/lib/src/main/java/br/com/tattobr/samples/framework/gl/TextureRegion.java
+++ game-framework/lib/src/main/java/br/com/tattobr/samples/framework/gl/TextureRegion.java
@@ -5,9 +5,13 @@ public class TextureRegion {
     public final float v1;
     public final float u2;
     public final float v2;
+    public final float width;
+    public final float height;
     public final Texture texture;
 
     public TextureRegion(Texture texture, float x, float y, float width, float height) {
+        this.width = width;
+        this.height = height;
         u1 = x / texture.getWidth();
         v1 = y / texture.getHeight();
         u2 = u1 + width / texture.getWidth();
